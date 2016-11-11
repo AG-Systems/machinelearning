860bec9c0dc3f382a00e1fd1c64528b48c95db85
Auriga <AG-Systems@users.noreply.github.com>
2016-11-11 13:26:41 -0800

    Changed the 0.01 to 0.10 percent & added comments

--- stock-learning/03_regression.py
+++ stock-learning/03_regression.py
@@ -18,13 +18,18 @@ df = df[['Adj. Open','Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume' ]]
 df['HL_PCT'] = (df['Adj. High']- df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_CHANGE'] = (df['Adj. Close']- df['Adj. Open']) / df['Adj. Open'] * 100.0
 
+#           price          x           x            x
 df = df[['Adj. Close', 'HL_PCT', 'PCT_CHANGE', 'Adj. Volume']]
+# Think about the features.
+# For example: Google is worth 500b because of the revuneve, value, etc etc.
+# Focus on that
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 # -9999 as a outline
 # Play around with 0.1
-forecast_out = int(math.ceil(0.01*len(df)))
+#forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
 # print(df.tail())
