dae35d0d440b1d5d849fcb1836a35fd728fa973d
Auriga <AG-Systems@users.noreply.github.com>
2016-11-10 23:35:37 -0800

    Create 02_regression.py

---
+++ stock-learning/02_regression.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import quandl
+import math
+import datetime
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+#graphing 
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+df = quandl.get('WIKI/GOOGL')
+
+df = df[['Adj. Open','Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume' ]]
+df['HL_PCT'] = (df['Adj. High']- df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_CHANGE'] = (df['Adj. Close']- df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_CHANGE', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+# -9999 as a outline
+# Play around with 0.1
+forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
+df['label'] = df[forecast_col].shift(-forecast_out)
+# print(df.tail())
+# head for when it first started and tail for most recent
+
+
+X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+# clf = svm.SVR()
+# clf = svm.SVR(kernel='poly')
+# svr is a worse algorithm. 
+# kernel is even worse
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test,y_test)
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecase'] = np.nan
+# graphing
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+    
+df['Adj. CLose'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
